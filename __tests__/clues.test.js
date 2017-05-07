0ecb064f1da257392c02014ed9fde03afcda0896
Bridie Dibble <bridie.dibble@gmail.com>
2017-05-07 10:24:49 +0100

    Adding test fixtures to make writing testing DRYer

--- __tests__/clues.test.js
+++ __tests__/clues.test.js
@@ -1,31 +1,11 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { shallow } from 'enzyme'
+import { currentTheme, acrossWords, downWords } from './fixtures/currentTheme';
 import Clues from './../src/components/clues';
 
 function setup() {
-	const props = {
-		currentTheme: {
-			"perro": {
-				"clue": "dog",
-				"number": 1,
-				"direction": "across",
-				"letters": [[1, 1], [2, 1], [3, 1], [4, 1], [5, 1]]
-			},
-			"rinocerente": {
-				"clue": "rhinoceros",
-				"number": 2,
-				"direction": "down",
-				"letters": [[3, 1], [3, 2], [3, 3], [3, 4], [3, 5], [3, 6], [3, 7], [3, 8], [3, 9], [3, 10], [3, 11]]
-			},
-			"cocodrilo": {
-				"clue": "crocodile",
-				"number": 3,
-				"direction": "across",
-				"letters": [[3, 5], [4, 5], [5, 5], [6, 5], [7, 5], [8, 5], [9, 5], [10, 5], [11, 5]]
-			}
-		}
-	}
+	const props = { currentTheme }
 
 	const enzymeWrapper = shallow(<Clues {...props} />)
 
@@ -45,37 +25,11 @@ describe("Clues Component", () => {
 
 	it('correctly filters the across words', () => {
 		const { enzymeWrapper } = setup();
-
-		const acrossWords = {
-			"cocodrilo": {
-				"clue": "crocodile",
-				"number": 3,
-				"direction": "across",
-				"letters": [[3, 5], [4, 5], [5, 5], [6, 5], [7, 5], [8, 5], [9, 5], [10, 5], [11, 5]]
-			},
-			"perro": {
-				"clue": "dog",
-				"number": 1,
-				"direction": "across",
-				"letters": [[1, 1], [2, 1], [3, 1], [4, 1], [5, 1]]
-			}
-		}
-
 		expect(enzymeWrapper.instance().getWordsWithDirection('across')).toEqual(acrossWords);
 	});
 
 	it('correctly filters the down words', () => {
 		const { enzymeWrapper } = setup();
-
-		const downWords = {
-			"rinocerente": {
-				"clue": "rhinoceros",
-				"number": 2,
-				"direction": "down",
-				"letters": [[3, 1], [3, 2], [3, 3], [3, 4], [3, 5], [3, 6], [3, 7], [3, 8], [3, 9], [3, 10], [3, 11]]
-			},
-		}
-
 		expect(enzymeWrapper.instance().getWordsWithDirection('down')).toEqual(downWords);
 	});
 })
--- __tests__/grid.test.js
+++ __tests__/grid.test.js
@@ -1,31 +1,11 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { shallow } from 'enzyme'
+import { currentTheme, sampleActiveCells, sampleInactiveCells } from './fixtures/currentTheme';	
 import Grid from './../src/components/grid';
 
 function setup() {
-	const props = {
-		currentTheme: {
-			"perro": {
-				"clue": "dog",
-				"number": 1,
-				"direction": "across",
-				"letters": [[1, 1], [2, 1], [3, 1], [4, 1], [5, 1]]
-			},
-			"rinocerente": {
-				"clue": "rhinoceros",
-				"number": 2,
-				"direction": "down",
-				"letters": [[3, 1], [3, 2], [3, 3], [3, 4], [3, 5], [3, 6], [3, 7], [3, 8], [3, 9], [3, 10], [3, 11]]
-			},
-			"cocodrilo": {
-				"clue": "crocodile",
-				"number": 3,
-				"direction": "across",
-				"letters": [[3, 5], [4, 5], [5, 5], [6, 5], [7, 5], [8, 5], [9, 5], [10, 5], [11, 5]]
-			}
-		}
-	}
+	const props = { currentTheme }
 
 	const enzymeWrapper = shallow(<Grid {...props} />)
 
@@ -46,11 +26,13 @@ describe("Grid Component", () => {
 	it('correctly specifies whether a cell is active or not', () => {
 		const { enzymeWrapper } = setup();
 
-		const notActiveCell = [2, 2];
-		const activeCell = [1, 1];
+		sampleActiveCells.forEach(sampleActiveCell => {
+			expect(enzymeWrapper.instance().isActive(...sampleActiveCell)).toEqual(true);
+		});
 
-		expect(enzymeWrapper.instance().isActive(...notActiveCell)).toEqual(false);
-		expect(enzymeWrapper.instance().isActive(...activeCell)).toEqual(true);
+		sampleInactiveCells.forEach(sampleInactiveCell => {
+			expect(enzymeWrapper.instance().isActive(...sampleInactiveCell)).toEqual(false);
+		});
 	});
 })
 
